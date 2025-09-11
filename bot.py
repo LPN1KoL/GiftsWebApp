f323808808245427877d3c4aa3c7ad40d119a488
LPN1KoL <nolsufa@gmail.com>
2025-09-11 18:21:34 +1000

    stop

--- bot.py
+++ bot.py
@@ -8,11 +8,14 @@ API_TOKEN = "8008525871:AAFpPTPQbsF661zdGXSNRsriquhiqn-VpKQ"
 
 
 async def main():
-    bot = Bot(token=API_TOKEN)
-    dp = Dispatcher()
-    dp.include_router(router)
-    await init_db()
-    await dp.start_polling(bot)
+    try:
+        bot = Bot(token=API_TOKEN)
+        dp = Dispatcher()
+        dp.include_router(router)
+        await init_db()
+        await dp.start_polling(bot)
+    except KeyboardInterrupt:
+        print("Bot stopped")
 
 if __name__ == "__main__":
     asyncio.run(main())
