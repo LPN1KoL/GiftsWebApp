754a1b306d4080f2e02ae570be3b9f045c869f72
LPN1KoL <nolsufa@gmail.com>
2025-09-11 19:44:44 +1000

    mb

--- start.py
+++ start.py
@@ -1,4 +1,4 @@
-from random import random
+import random
 from fastapi import FastAPI, HTTPException, Request
 from fastapi.staticfiles import StaticFiles
 from fastapi.middleware.cors import CORSMiddleware
