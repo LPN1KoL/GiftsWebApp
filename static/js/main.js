e0434f53e4e83b81ba5e2de88e4f05fb0e5cf71c
LPN1KoL <nolsufa@gmail.com>
2025-09-18 21:17:39 +1000

    certs

--- static/js/main.js
+++ static/js/main.js
@@ -193,12 +193,14 @@ function sleep(ms) {
 async function sell_gift(gift_id){
 
     try {
-        const result = await sendApiRequest('/api/sell_gift', { initData, gift_id });
+        const tg = window.Telegram?.WebApp;
+        const result = await sendApiRequest('/api/sell_gift', { initData: tg.initData, gift_id: gift_id });
         if (result && result.success) {
             document.querySelector('.modal').classList.remove('active');
             return;
         } else {
             alert('Ошибка при продаже подарка');
+            console.error("Ошибка при продаже подарка:", result);
             return;
         }
 
